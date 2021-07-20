8e2081e8781bca7ae28934ba17a7f0df41dff738
Jason Tang <tangmiao7@163.com>
2021-07-20 19:53:05 +0800

    update 添加飞船图片

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     #初始化游戏，并创建一个屏幕对象
@@ -14,6 +15,9 @@ def run_game():
     #设置背景颜色
     bg_color = ai_setting.bg_color
 
+    #创建一艘飞船
+    ship = Ship(screen)
+
     #开始游戏的主循环
     while True:
 
@@ -24,6 +28,7 @@ def run_game():
         
         #每次循环时都重绘屏幕
         screen.fill(bg_color)
+        ship.blitme()
 
         #让最近绘制的屏幕可见
         pygame.display.flip()
