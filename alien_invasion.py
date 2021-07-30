62fe7b4cfe685c6f07e1850a5167e55c16183ada
JaonTang7 <tangmiao7@163.com>
2021-07-30 17:28:04 +0800

    一些小改动

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 
 def run_game():
@@ -22,6 +23,9 @@ def run_game():
     #创建一个用于存储子弹的编组
     bullets = Group()
 
+    #创建一个外星飞船
+    alien = Alien(ai_setting,screen)
+
     #开始游戏的主循环
     while True:
 
@@ -32,7 +36,7 @@ def run_game():
         #更新子弹
         gf.update_bullets(bullets)
         #重绘屏幕
-        gf.update_screen(ai_setting,screen,ship,bullets)
+        gf.update_screen(ai_setting,screen,ship,bullets,alien)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -44,7 +44,7 @@ def check_keyup_events(event,ship):
         ship.moving_down = False
 
 
-def update_screen(ai_setting,screen,ship,bullets):
+def update_screen(ai_setting,screen,ship,bullets,alien):
     """更新屏幕上的图像，并切换到新屏幕"""
     #每次循环时都重绘屏幕
     screen.fill(ai_setting.bg_color)
@@ -52,6 +52,7 @@ def update_screen(ai_setting,screen,ship,bullets):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
+    alien.blitme()
     #让最近绘制的屏幕可见
     pygame.display.flip()
 
