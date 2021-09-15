50395443a6588f32fc807e6f3b2ebcb5c984d1b7
Jason Tang <tangmiao7@163.com>
2021-09-15 08:05:22 +0800

    创建一行外星飞船

--- game_functions.py
+++ game_functions.py
@@ -4,6 +4,7 @@ import pygame
 from bullet import Bullet
 from settings import Settings
 from ship import Ship
+from alien import Alien
 
 def check_events(ai_settings,screen,ship,bullets):
     """响应按键和鼠标事件"""
@@ -44,7 +45,7 @@ def check_keyup_events(event,ship):
         ship.moving_down = False
 
 
-def update_screen(ai_setting,screen,ship,bullets,alien):
+def update_screen(ai_setting,screen,ship,bullets,aliens):
     """更新屏幕上的图像，并切换到新屏幕"""
     #每次循环时都重绘屏幕
     screen.fill(ai_setting.bg_color)
@@ -52,7 +53,7 @@ def update_screen(ai_setting,screen,ship,bullets,alien):
     for bullet in bullets.sprites():
         bullet.draw_bullet()
     ship.blitme()
-    alien.blitme()
+    aliens.draw(screen)
     #让最近绘制的屏幕可见
     pygame.display.flip()
 
@@ -72,3 +73,21 @@ def fire_bullet(ai_settings,screen,ship,bullets):
     if len(bullets) <= ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings,screen,ship)
         bullets.add(new_bullet)
+
+
+def create_fleet(ai_settings,screen,aliens):
+    """创建外星人舰队"""
+    #创建一个外星飞船，并计算一行可容纳多少个外星飞船
+    #外星飞船间距为外星飞船宽度
+    alien = Alien(ai_settings,screen)
+    alien_width = alien.rect.width
+    avaliable_space_X = ai_settings.screen_width - 2*alien_width
+    number_aliens_x = int(avaliable_space_X / (2*alien_width))
+
+    #创建第一行外星人
+    for alien_number in range(number_aliens_x):
+        #创建一个外星人并将其加入当前行
+        alien = Alien(ai_settings,screen)
+        alien.x = alien_width + 2*alien_width*alien_number
+        alien.rect.x = alien.x
+        aliens.add(alien)
